8de69427b36b6df0876d5dc15ab6062380abfece
Arturo Guerra <ar2roguerra@gmail.com>
2018-03-31 04:56:11 +0000

    S3

--- src/arturoimages.js
+++ src/arturoimages.js
@@ -9,21 +9,27 @@ const path = require('path');
 const multer = require("multer");
 const multerS3 = require("multer-s3");
 const bodyParser = require('body-parser');
+const config = require('./config.json');
 
-
-aws.config.update({region: "us-east-1"});
+aws.config.update({region: config.region});
 const app = new express();
-const s3 = new new aws.S3();
+const s3 = new aws.S3();
 
+function FileValidation(ext) {
+    const validfiles = ['png', 'jpg', 'jpeg', 'gif'];
+    return validfiles.some((element, index, array) => {
+        return element === ext;
+    });
+}
 
 const storage = multerS3({
     s3: s3,
-    bucket: "img-dixionary",
+    bucket: config.bucket,
     acl: "public-read",
     metadata: function(req, file, cb) {
         cb(null, {fieldName: file.fieldname});
     },
-    key: function(req, files, cb) {
+    key: function(req, file, cb) {
         let filename_noext = uuid4().replace(/-/g, "").substring(0,2) + Date.now();
         let ext = file.mimetype.split('/')[1];
         let filename = filename_noext + "." + ext;
@@ -31,14 +37,25 @@ const storage = multerS3({
     }
 })
 
-const upload = multer({limits: {fileSize: 100000000}, storage: storage});
+const upload = multer({
+    storage: storage,
+    limits: {fileSize: 100000000},
+    fileFilter: function(req, file, cb) {
+        let ext = file.mimetype.split('/')[1];
+        if (FileValidation(ext)) {
+            cb(null, true);
+        }
+        else {
+            cb(new Error("Only images are allowed"));
+        }
+    }
+})
 
 app.use(compression());
 app.use(bodyParser.urlencoded({extended: false, limit: "100mb"}));
 app.use(bodyParser.json({limit: "100mb"}));
 app.use(bodyParser.text({limit: "100mb"}));
 app.use('/static', express.static(path.join(__dirname, "static")));
-app.use('/images', express.static(path.join(__dirname, "images")));
 app.set('views', path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
 
@@ -53,24 +70,22 @@ app.use("/image/:image", (req, res, next) => {
     if (!req.params.image) {
         res.status(400).send("Invalid file");
     } else {
-        let img = {
-            image: "https://s3.amazonaws.com/img-dixionary/" + req.params.image,
-            name: req.params.image
-        }
-        res.render("image", img);
+        let img = {};
+        img.name = req.params.image;
+        s3.getObject({Bucket: config.bucket, Key: req.params.image}, function(err, data) {
+            if (err) {
+                img.image = config.coldstore + "/" + req.params.image
+            } else {
+                img.image = "https://s3.amazonaws.com/"+ config.bucket + "/" + req.params.image
+            }
+            res.render("image", img);
+        })
     }
 });
 
 app.post("/upload", upload.single('image'), (req, res, next) => {
-    res.send('/image/' + req.file.filename)
+    res.send('/image/' + req.file.key)
 });
-
-function FileValidation(ext) {
-    const validfiles = ['png', 'jpg', 'jpeg', 'gif'];
-    return validfiles.some((element, index, array) => {
-        return element === ext;
-    });
-}
 
 function startServer() {
     // Creates unix socket
